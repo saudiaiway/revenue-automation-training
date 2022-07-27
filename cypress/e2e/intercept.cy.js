f62ca8ae4e9f477a10e8244db5ddb79b5f0e4463
detsvdr <odette@kumu.ph>
2022-07-27 16:15:22 +0800

    ODETTE: Add intercept.js file

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -1,1 +1,16 @@
 /// <reference types="Cypress" />
+
+describe("Saple Test", () => {
+    it("intercept a request", () => {
+        cy.visit("https://jsonplaceholder.typicode.com");
+        cy.intercept({
+            path: "/posts",
+        }).as("posts");
+        cy.get("table:nth-of-type(1) a[href='/posts']").click();
+        cy.wait("@posts").then((inter) => {
+            cy.log(JSON.stringify(inter));
+            console.log(inter);
+            expect(inter.response.body).to.have.length(100);
+        })
+    }) 
+})
